b7607229ae856db0c44da5e3d6c74b3c8ec0972a
Mr G Brooks <49302399+grbrooks@users.noreply.github.com>
2021-08-03 16:55:08 +0100

    my first php file

--- index.php
+++ index.php
@@ -11,8 +11,15 @@
 <body>
     <?php include('templates/header.php');?>
     <?php include('templates/menu.php');?>
-    <h1>Title</h1>
-    <p>Sample content here</p>
+    <?php
+    $query =mysqli_query($sql,"SELECT * FROM tbl_articles");
+    while($row =mysqli_fetch_assoc($query))
+    {
+        $title = $row['title'];
+        $content = $row['content'];
+    }?>
+    <h1><?php echo $title; ?></h1>
+    <p><?php echo $content; ?></p>
     <?php include('templates/footer.php');?>
 </body>
 </html>
